ddc50b7cd39a8994c7122fe73e5131b2f6848ae5
Jomipon <tomas.vlasaty@centrum.cz>
2025-10-24 17:27:26 +0200

    test

--- main.py
+++ main.py
@@ -41,17 +41,9 @@ st.set_page_config(page_title="LEJSEK")
 
 session = None
 
-def write_email(session):
-    try:
-        st.write(session.user.email)
-    except:
-        st.write("None")
 set_session_from_params(st.session_state["sb_database"])
-write_email(session)
 session = get_session_from_session_state(session, st.session_state["sb_database"], cookies)
-write_email(session)
 session = get_session_from_cookies(session, st.session_state["sb_database"], cookies)
-write_email(session)
 
 if session:
     col_picture, col_login = st.columns(2)
